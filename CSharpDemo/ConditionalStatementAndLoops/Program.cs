3e60f9526b10b1ba5129e150a5ef71f0232208b5
edwin444 <edwinthomas444@gmail.com>
2019-04-29 14:23:02 +0400

    Added loops

--- CSharpDemo/ConditionalStatementAndLoops/Program.cs
+++ CSharpDemo/ConditionalStatementAndLoops/Program.cs
@@ -4,14 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace VariablesDemo
+namespace ConditionalStatementAndLoops
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int temp = 10;
-            Console.WriteLine($"First C# pgm id: {temp}");
         }
     }
 }
---
+++ CSharpDemo/VariablesDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //specifying the variable type
+            int temp = 10;
+            Console.WriteLine($"First C# pgm id: {temp}");
+
+            //not specifying the variable type
+            var temp1 = "Edwin";
+            Console.WriteLine($"My name is : {temp1}");
+        }
+    }
+}
